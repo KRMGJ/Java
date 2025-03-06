02367cebd3dac6234f1fc4963fcee86fc4ec56f0
orderabbit <dur369667@naver.com>
2025-03-06 17:44:04 +0900

    day1

---
+++ src/CircleParameter.java
@@ -0,0 +1,8 @@
+public class CircleParameter{
+    public static void main(String[] args) {
+        final double PI = 3.14;
+        double radius = 5.0;
+        double circleParameter = 2 * radius * PI;
+        System.out.println("원의 둘레 = " + circleParameter);
+    }
+}
